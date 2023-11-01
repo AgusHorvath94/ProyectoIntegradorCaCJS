a0a10fc0799fafefbdcb0d3313b62b3831e530fa
AgusHorvath94 <agushorvath994@gmail.com>
2023-11-01 20:26:20 -0300

    mas funcionalidades

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
 
+
+//--> Le pongo 1 por defecto a la cantidad <--//
+
+document.addEventListener("DOMContentLoaded", function() 
+{
+  const inputCant = document.getElementById('inputCant');
+  inputCant.value = '1';
+});
+
+//--> Cuando abro el documento hace foco en el primer input de nombre <--//
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+  const inputName = document.getElementById('inputName');
+
+  inputName.focus();
+});
+
+//--> No dejo que se carguen datos que no sean numeros en la cantidad <--//
+
+
+document.addEventListener("DOMContentLoaded", function() 
+{
+  const inputCant = document.getElementById('inputCant');
+
+  inputCant.addEventListener('input', function(event) {
+    const inputValue = inputCant.value;
+
+    if (!/^\d*$/.test(inputValue)) {
+      inputCant.value = inputValue.slice(0, -1);
+    }
+  });
+});
+
+
 //--> Funcion para calcular total <--//
 
 function calcularTotal() {
@@ -16,22 +51,47 @@ function calcularTotal() {
   var valorTicket = 200;
 
   var total = cantidad * (1 - descuentos[categoria]) * valorTicket;
-  total = Math.round(total);
+  total = total.toFixed(2);
 
   document.getElementById("inputCeleste").textContent =
     "Total a pagar: $" + total;
 }
 
+
+//--> Me fijo que la cantidad sea valida antes de calcular el total <--//
+
+document.getElementById("botonSubmit").addEventListener("click", function (e) 
+{
+  var cantidad = document.getElementById("inputCant").value;
+
+  if (isNaN(cantidad) || parseInt(cantidad, 10) <= 0) 
+  {
+    alert("La cantidad debe ser un número mayor que 0");
+    e.preventDefault();
+  } 
+  else 
+  {
+    calcularTotal();
+  }
+});
+
+
 //--> Eventos para calcular total y resetear el formulario <--//
 
   document.getElementById("botonSubmit").addEventListener("click", function (e) {
+   
     e.preventDefault(); 
+   
     calcularTotal(); 
+ 
   });
 
   document.getElementById("botonReset").addEventListener("click", function () {
-  
-  document.getElementById("inputCeleste").textContent = "Total a pagar: $0.00";
+
+      document.getElementById("inputCeleste").textContent = "Total a pagar: $0.00";
+      const inputName = document.getElementById('inputName');
+      inputName.focus();
 
 });
+
 
